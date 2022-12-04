66af0982cbe162953da24ef7e637786b79673b10
Ahhh_Saturn <saturnmaoudis@protonmail.com>
2022-12-04 13:12:15 +0000

    removed arrIndex as its not needed

--- index.js
+++ index.js
@@ -13,28 +13,18 @@ exports.Database = function Database(location, databaseName) {
 			file = require(filePath);
 		} else if (String(err.code).search('Error: Cannot find module')) throw new Error(`Cound not find ${databaseName}.json, is it in ${filePath}`);
 	}
-	this.read = (index, arrIndex) => {
+	this.read = (index) => {
 		if (index === undefined) return file;
 		if (file[index] === undefined) throw new Error(`Could not find index '${index}' in ${databaseName} database`);
-		if (arrIndex !== undefined) return file[index][arrIndex];
 		return file[index];
 	};
 	this.write = (index, value) => {
 		file[index] = value;
 		fs.writeFileSync(filePath, JSON.stringify(file));
 	};
-	this.remove = (index, arrIndex) => {
+	this.remove = (index) => {
 		if (file[index] === undefined) throw new Error(`Could not find index '${index}' in ${databaseName} database`);
-		if (typeof arrIndex !== 'undefined') {
-			let indexLocation = file[index].findIndex((element) => element == arrIndex);
-			console.log(file[index]);
-			if (indexLocation == -1) throw new Error(`Could not find ${arrIndex} in index '${index}' in ${databaseName} database`);
-			file[index].splice(indexLocation, 1);
-			console.log(file[index]);
-			fs.writeFileSync(filePath, JSON.stringify(file));
-		} else {
-			delete file[index];
-			fs.writeFileSync(filePath, JSON.stringify(file));
-		}
+		delete file[index];
+		fs.writeFileSync(filePath, JSON.stringify(file));
 	};
 };
